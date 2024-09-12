524caffd31532e8ecd05f7443986c53b172aa105
omkute <58985673+omkute@users.noreply.github.com>
2024-09-12 15:19:42 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import NavBar from "@/components/NavBar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className=" h-[100vh] w-full bg-white">
+      
+    </div>
+  );
+}
